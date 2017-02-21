08fd3e76c7511022754d32f7e747ab4ff3b8f16e
Christopho <christopho@solarus-games.org>
2017-02-21 15:52:32 +0100

    Add Solarus team logo

--- data/main.lua
+++ data/main.lua
@@ -20,7 +20,8 @@ function sol.main:on_started()
   -- If there is a file called "debug" in the write directory, enable debug mode.
   debug_enabled = sol.file.exists("debug")
 
-  local solarus_logo = require("menus/solarus_logo")
+  local solarus_logo = require("scripts/menus/solarus_logo")
+  local team_logo = require("scripts/menus/team_logo")
   local language_menu = require("menus/language")
   local title_screen = require("menus/title")
   local savegame_menu = require("menus/savegames")
@@ -28,8 +29,15 @@ function sol.main:on_started()
   -- Show the Solarus logo first.
   sol.menu.start(self, solarus_logo)
 
-  -- Then the language selection menu, unless a game was started by a debug key.
+  -- Then the Solarus team logo.
   solarus_logo.on_finished = function()
+    if self.game == nil then
+      sol.menu.start(self, team_logo)
+    end
+  end
+
+  -- Then the language selection menu.
+  team_logo.on_finished = function()
     if self.game == nil then
       sol.menu.start(self, language_menu)
     end
