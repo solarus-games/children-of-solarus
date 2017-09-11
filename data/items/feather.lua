554ebe283c7ac33f9bdbd6b399676b274d118dfd
Diarandor <diarandor@gmail.com>
2017-09-11 03:19:22 +0200

    Add custom jump script

--- data/items/feather.lua
+++ data/items/feather.lua
@@ -1,15 +1,169 @@
 local item = ...
 
+local hero_meta = sol.main.get_metatable("hero")
+
+-- Initialize parameters for custom jump.
+local is_hero_jumping = false
+local jump_duration = 600 -- Duration of jump in milliseconds.
+local max_height = 16 -- Height of jump in pixels.
+local max_distance = 31 -- Max distance of jump in pixels.
+local jumping_speed = math.floor(1000 * max_distance / jump_duration)
+
 function item:on_created()
   self:set_savegame_variable("i1100")
   self:set_assignable(true)
 end
 
 function item:on_using()
-  sol.audio.play_sound("jump")
-  local hero = self:get_map():get_entity("hero")
-  local direction4 = hero:get_direction()
-  hero:start_jumping(direction4 * 2, 32, false)
-  self:set_finished()
+  local hero = self:get_game():get_hero()
+  if item:get_variant() == 1 then -- Built-in jump.
+    sol.audio.play_sound("jump")
+    local direction4 = hero:get_direction()
+    hero:start_jumping(direction4 * 2, 32, false)
+    self:set_finished()
+  else -- Custom jump.
+    hero:start_custom_jump()
+  end
 end
 
+-- Used to detect if custom jump is being used.
+-- Necessary to determine if other items can be used.
+function hero_meta:is_jumping()
+  return is_hero_jumping
+end
+
+-- Function to determine if the hero can jump on this type of ground.
+local function is_jumpable_ground(ground_type)
+  return (
+    (ground_type == "traversable")
+    or (ground_type == "wall_top_right") or (ground_type == "wall_top_left")
+    or (ground_type == "wall_bottom_left") or (ground_type == "wall_bottom_right")
+    or (ground_type == "shallow_water") or (ground_type == "grass")
+    or (ground_type == "ice")
+  )
+end
+
+-- Define custom jump on hero metatable.
+function hero_meta:start_custom_jump()
+
+  local hero = self
+  local game = self:get_game()
+  local map = self:get_map()
+  local item = game:get_item("feather")
+
+  -- Do nothing if the hero is frozen, carrying, "custom jumping",
+  -- or if there is bad ground below. [Add more restrictions if necessary.]
+  local hero_state = hero:get_state()
+  local is_hero_frozen = hero_state == "frozen"
+  local is_hero_carrying = hero_state == "carrying"
+  local ground_type = map:get_ground(hero:get_position())
+  local is_ground_jumpable = is_jumpable_ground(ground_type)
+
+  if is_hero_frozen or is_hero_jumping or is_hero_carrying or (not is_ground_jumpable) then
+    return
+  end
+
+  -- Prepare hero for jump.
+  hero:unfreeze()  
+  is_hero_jumping = true
+  hero:save_solid_ground(hero:get_position()) -- Save solid position.
+  local ws = hero:get_walking_speed() -- Default walking speed.
+  hero:set_walking_speed(jumping_speed)
+  hero:set_invincible(true, jump_duration)
+  sol.audio.play_sound("jump")
+ 
+  -- Change and fix tunic animations to display the jump.
+  local state = hero:get_state()
+  if state == "free" then
+    hero:set_fixed_animations("jumping", "jumping")
+    hero:set_animation("jumping")
+  elseif state == "sword loading" then
+    hero:set_fixed_animations("sword_loading_stopped", "sword_loading_stopped")
+  elseif state == "sword spin attack" then
+    hero:set_fixed_animations("spin_attack", "spin_attack")
+  end
+
+  -- Create shadow platform with traversable ground that follows the hero under him.
+  local x, y, layer = hero:get_position()
+  local platform_properties = {x=x,y=y,layer=layer,direction=0,width=8,height=8}
+  local tile = map:create_custom_entity(platform_properties)
+  tile:set_origin(4, 4)
+  tile:set_modified_ground("traversable")
+  local sprite = tile:create_sprite("shadows/shadow_big_dynamic")
+  local nb_frames = sprite:get_num_frames()
+  local frame_delay = math.floor(jump_duration/nb_frames)
+  sprite:set_frame_delay(frame_delay)
+  -- Shadow platform has to follow the hero.
+  sol.timer.start(tile, 1, function()
+    tile:set_position(hero:get_position())
+    return true
+  end)
+
+  -- Shift all sprites during jump with parabolic trajectory.
+  local instant = 0
+  sol.timer.start(self, 1, function()
+    if not is_hero_jumping then return false end
+    local tn = instant/jump_duration
+    local height = math.floor(4*max_height*tn*(1-tn))
+    for _, s in hero:get_sprites() do
+      s:set_xy(0, -height)
+    end
+    -- Continue shifting while jumping.
+    instant = instant + 1
+    return true
+  end)
+
+  -- Finish the jump.
+  sol.timer.start(item, jump_duration, function()
+    hero:set_walking_speed(ws) -- Restore initial walking speed.
+    hero:set_fixed_animations(nil, nil) -- Restore tunic animations.
+    tile:remove()  -- Delete shadow platform tile.
+    -- If ground is empty, move hero to lower layer.
+    local x,y,layer = hero:get_position()
+    local ground = map:get_ground(hero:get_position())
+    local min_layer = map:get_min_layer()
+    while ground == "empty" and layer > min_layer do
+      layer = layer-1
+      hero:set_position(x,y,layer)
+      ground = map:get_ground(hero:get_position())    
+    end
+    -- Reset sprite shifts.
+    for _, s in hero:get_sprites() do s:set_xy(0, 0) end
+    
+    -- Create ground effect.
+    -- item:create_ground_effect(x, y, layer)
+
+    -- Restore solid ground as soon as possible.
+    sol.timer.start(self, 1, function()
+      local ground_type = map:get_ground(hero:get_position())    
+      local is_good_ground = is_jumpable_ground(ground_type)
+      if is_good_ground then
+        hero:reset_solid_ground()
+        return false
+      end
+      return true
+    end)   
+
+    -- Finish jump.
+    is_hero_jumping = false
+    item:set_finished()
+  end)
+end
+
+-- Create ground effects for hero landing after jump.
+-- TODO: DELETE THIS FUNCTION AND USE THE ONE IN "SCRIPTS/GROUND_EFFECTS.LUA"
+function item:create_ground_effect(x, y, layer)
+
+  local map = item:get_map()
+  local ground = map:get_ground(x, y, layer)
+  if ground == "deep_water" or ground == "shallow_water" then
+    -- If the ground has water, create a splash effect.
+    map:create_ground_effect("water_splash", x, y, layer, "splash")
+  elseif ground == "grass" then
+    -- If the ground has grass, create leaves effect.
+    map:create_ground_effect("falling_leaves", x, y, layer, "bush")
+  else
+    -- For other grounds, make landing sound.
+    sol.audio.play_sound("hero_lands")      
+  end
+end
--- data/scripts/meta/hero.lua
+++ data/scripts/meta/hero.lua
@@ -6,6 +6,92 @@ function hero_meta:on_created()
 
   local hero = self
   hero:set_tunic_sprite_id("hero/eldran")
+  hero:initialize_fixing_functions() -- Used to fix direction and animations.
 end
+
+--------------------------------------------------
+-- Functions to fix tunic animation and direction.
+--------------------------------------------------
+local fixed_direction, fixed_stopped_animation, fixed_walking_animation
+
+-- Return true if the hero is walking.
+function hero_meta:is_walking()
+
+  local m = self:get_movement()
+  return m and m.get_speed and m:get_speed() > 0
+end
+
+-- Get fixed direction for the hero.
+function hero_meta:get_fixed_direction()
+
+  return fixed_direction
+end
+
+-- Get fixed stopped/walking animations for the hero.
+function hero_meta:get_fixed_animations()
+
+  return fixed_stopped_animation, fixed_walking_animation
+end
+
+-- Set a fixed direction for the hero (or nil to disable it).
+function hero_meta:set_fixed_direction(new_direction)
+
+  fixed_direction = new_direction
+  if fixed_direction then self:get_sprite("tunic"):set_direction(fixed_direction) end
+end
+
+-- Set fixed stopped/walking animations for the hero (or nil to disable them).
+function hero_meta:set_fixed_animations(new_stopped_animation, new_walking_animation)
+
+  fixed_stopped_animation = new_stopped_animation
+  fixed_walking_animation = new_walking_animation
+  -- Initialize fixed animations if necessary.
+  local state = self:get_state()
+  if state == "free" then
+    if self:is_walking() then self:set_animation(fixed_walking_animation or "walking")
+    else self:set_animation(fixed_stopped_animation or "stopped") end
+  end
+end
+
+-- Initialize events to fix direction and animation for the tunic sprite of the hero.
+-- For this purpose, we redefine on_created and set_tunic_sprite_id events for the hero metatable.
+function hero_meta:initialize_fixing_functions()
+
+  local hero = self
+  local sprite = hero:get_sprite("tunic")
+
+  -- Define events for the tunic sprite.
+  function sprite:on_animation_changed(animation)
+    local tunic_animation = sprite:get_animation()
+    if tunic_animation == "stopped" and fixed_stopped_animation ~= nil then 
+      if fixed_stopped_animation ~= tunic_animation then
+        sprite:set_animation(fixed_stopped_animation)
+      end
+    elseif tunic_animation == "walking" and fixed_walking_animation ~= nil then 
+      if fixed_walking_animation ~= tunic_animation then
+        sprite:set_animation(fixed_walking_animation)
+      end
+    --elseif tunic_animation == "pushing" then
+      --local pushing_animation = hero:get_pushing_animation()
+      --if pushing_animation then hero:set_animation(pushing_animation) end
+    --end
+  end
+  function sprite:on_direction_changed(animation, direction)
+    local fixed_direction = hero.fixed_direction
+    local tunic_direction = sprite:get_direction()
+    if fixed_direction ~= nil and fixed_direction ~= tunic_direction then
+      sprite:set_direction(fixed_direction)
+    end
+  end
+end
+
+-- Initialize fixing functions for the new sprite when the tunic sprite is changed.
+local old_set_tunic = hero_meta.set_tunic_sprite_id -- We redefine this function.
+function hero_meta:set_tunic_sprite_id(sprite_id)
+    old_set_tunic(self, sprite_id)
+    self:initialize_fixing_functions()
+  end
+end
+
 
 return true
