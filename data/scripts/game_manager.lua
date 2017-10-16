79b703334d734854b15ca9b2fa96130cefe2d614
Diarandor <diarandor@gmail.com>
2017-10-16 13:34:08 +0200

    Modify scripts for event on_world_changed
    The code has to be like in the project OLB

--- data/scripts/game_manager.lua
+++ data/scripts/game_manager.lua
@@ -57,4 +57,22 @@ function game_manager:create(file)
   return game
 end
 
+-- TODO the engine should have an event game:on_world_changed().
+local game_meta = sol.main.get_metatable("game")
+game_meta:register_event("on_map_changed", function(game)
+
+  local map = game:get_map()
+  local new_world = map:get_world()
+  local previous_world = game.previous_world
+  local world_changed = previous_world == nil or
+      new_world == nil or
+      new_world ~= previous_world
+  game.previous_world = new_world
+  if world_changed then
+    if game.notify_world_changed ~= nil then
+      game:notify_world_changed(previous_world, new_world)
+    end
+  end
+end)
+
 return game_manager
