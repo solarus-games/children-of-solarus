d975f93a8decf43d496e4ae434e8d17d61955a65
Diarandor <diarandor@gmail.com>
2017-11-12 10:59:24 +0100

    Update rain script
    Use oclero's suggestions:
    -Recolor: rain and lightnings are now white.
    -Water has more transparency.
    -Use darkness surface during storm mode.
    -Lightning and darkness surfaces use blend modes.

--- data/scripts/weather/rain_manager.lua
+++ data/scripts/weather/rain_manager.lua
@@ -1,5 +1,9 @@
 -- Rain manager script.
---[[
+-- Author: Diarandor (Solarus Team).
+-- License: GPL v3-or-later.
+-- Donations: solarus-games.org, diarandor at gmail dot com.
+
+--[[   Instructions:
 To add this script to your game, call from game_manager script:
     require("scripts/weather/rain_manager")
 
@@ -34,9 +38,12 @@ local drop_max_distance = 300 -- Max possible distance for drop movements.
 local rain_drop_delay = 10 -- Delay between drops for rain, in milliseconds.
 local storm_drop_delay = 5 -- Delay between drops for storms, in milliseconds.
 local current_drop_delay
-local min_lightning_delay = 2000
-local max_lightning_delay = 10000
-local rain_surface, flash_surface -- Surfaces to draw rain and lightning flash.
+local min_lightning_delay = 3000
+local max_lightning_delay = 15000
+local rain_surface, flash_surface, dark_surface -- Surfaces.
+local min_darkness, max_darkness = 120, 170 -- Opacity during storm.
+local current_darkness = 0 -- Opacity (transparent = 0).
+local color_darkness = {120, 120, 200} -- Used for full darkness.
 local draw_flash_surface = false -- Used by the lightning menu.
 local current_drop_index = 0 -- Current index for the next drop to be created.
 local max_drop_number_rain = 120
@@ -46,7 +53,7 @@ local drop_list = {} -- List of properties for each drop.
 local splash_list = {} -- List of properties for each splash effect.
 local timers = {}
 local num_drops, num_splashes = 0, 0
-local current_map, current_rain_mode
+local current_map, current_rain_mode, previous_rain_mode
 
 -- Initialize rain on maps when necessary.
 game_meta:register_event("on_map_changed", function(game)
@@ -56,14 +63,16 @@ end)
 
 -- Create rain if necessary when entering a new map.
 function rain_manager:on_map_changed()
-  -- Clear variables.
-  rain_surface, flash_surface, draw_flash_surface = nil, nil, nil
-  drop_list, splash_list, timers = {}, {}, {}
-  num_drops, num_splashes = 0, 0
   -- Get rain state in this world.
   local map = current_map
   local world = map:get_world()
   local rain_mode = map:get_game():get_world_rain_mode(world)
+  if current_rain_mode == rain_mode and rain_mode == nil then return end
+  -- Clear variables.
+  rain_surface, flash_surface, dark_surface = nil, nil, nil
+  draw_flash_surface = nil
+  drop_list, splash_list, timers = {}, {}, {}
+  num_drops, num_splashes = 0, 0
   -- Start rain if necessary.
   self:start_rain_mode(rain_mode)
   -- Draw rain: start menu.
@@ -112,6 +121,9 @@ function rain_manager:on_draw(dst_surface)
     end
     -- Draw the surface.
     rain_surface:draw(dst_surface) -- Draw rain.
+  end
+  if dark_surface then
+    dark_surface:draw(dst_surface) -- Draw dark surface.
   end
   if draw_flash_surface and flash_surface then
     flash_surface:draw(dst_surface) -- Draw lightning if necessary.
@@ -171,9 +183,9 @@ function rain_manager:create_splash(index)
 end
 
 -- Stop rain effects for the current map.
-function rain_manager:stop()
+function rain_manager:stop(timers_list)
   -- Stop drop rain timers if already started.
-  for _, key in pairs({"drop_timer", "lightning_timer"}) do
+  for _, key  in pairs(timers_list) do
     local t = timers[key]
     if t then t:stop() end
     timers[key] = nil
@@ -184,12 +196,12 @@ end
 -- Start rain in the current map.
 function rain_manager:start_rain_mode(rain_mode)
   -- Reset drop timer. Do nothing unless necessary.
-  if current_rain_mode == rain_mode then return end
-  self:stop()
+  previous_rain_mode = current_rain_mode
   current_rain_mode = rain_mode
-  if rain_mode == nil or (not rain_enabled) then return end
   -- Reset other timers.
-  for _, t in pairs(timers) do t:stop() end
+  self:stop({"drop_timer", "lightning_timer"})
+  self:update_darkness() -- Update darkness (fade-out effects included).
+  if rain_mode == nil or (not rain_enabled) then return end
   -- Initialize parameters.
   local drop_deviation = 0
   if rain_mode == "rain" then
@@ -200,20 +212,27 @@ function rain_manager:start_rain_mode(rain_mode)
     current_drop_speed = storm_speed
     current_drop_delay = storm_drop_delay
     max_drop_number = max_drop_number_storm
-  else
+  elseif rain_mode ~= nil then
     error("Invalid rain mode.")
   end
-  -- Create rain and lightning surfaces.
+  -- Create rain, dark and lightning surfaces.
   local map = current_map
   local camera = map:get_camera()
   local cx, cy, cw, ch = camera:get_bounding_box()
   rain_surface = sol.surface.create(cw, ch)
   if rain_mode == "storm" then
+    -- Lightnings.
     flash_surface = sol.surface.create(cw, ch)
-    flash_surface:fill_color({255, 255, 100})
+    flash_surface:fill_color({250, 250, 250})
     flash_surface:set_opacity(170)
+    flash_surface:set_blend_mode("add")
     self:start_lightnings()
-  else flash_surface = nil end
+    -- Darkness.
+    dark_surface = sol.surface.create(cw, ch)
+    dark_surface:set_blend_mode("multiply")  
+  else
+    flash_surface = nil  
+  end
   -- Initialize drop timer.
   timers["drop_timer"] = sol.timer.start(map, current_drop_delay, function()
     -- Check if there is space for a new drop (there is a max number of drops).
@@ -227,6 +246,8 @@ function rain_manager:start_rain_mode(rain_mode)
     end
     return true -- Repeat loop.
   end)
+  -- Restart other timers.
+  self:stop({"drop_frame_timer", "splash_frame_timer"})
   -- Update rain frames for all drops at once.
   timers["drop_frame_timer"] = sol.timer.start(map, 75, function()
     for _, drop in pairs(drop_list) do
@@ -259,8 +280,7 @@ end
 function rain_manager:start_lightnings()
   -- Play thunder sound after a random delay.
   local map = current_map
-  local t = timers["lightning_timer"]
-  if t then t:stop() end
+  self:stop({"lightning_timer"})
   local function create_next_lightning()
     local lightning_delay = math.random(min_lightning_delay, max_lightning_delay)
     timers["lightning_timer"] = sol.timer.start(map, lightning_delay, function()
@@ -284,6 +304,39 @@ function rain_manager:start_lightnings()
   end
   -- Start loop of lightnings.
   create_next_lightning()
+end
+
+-- Fade in/out dark surface for storm mode. Parameter (opacity) is optional.
+function rain_manager:update_darkness()
+  -- Define next darkness value.
+  local darkness = 0 
+  if current_rain_mode == "storm" then
+    darkness = math.random(min_darkness, max_darkness)
+  end
+  local d = 0 -- Increment/decrement for opacity.
+  if darkness > current_darkness then d = 1
+  elseif darkness < current_darkness then d = -1 end
+  self:stop({"darkness_timer"}) -- Destroy old timer.
+  -- Start modifying darkness towards the next value.
+  timers["darkness_timer"] = sol.timer.start(current_map, 15, function()
+    if dark_surface == nil then return false end
+    current_darkness = current_darkness + d
+    local r = 255 - math.floor(color_darkness[1] * (current_darkness / 255))
+    local g = 255 - math.floor(color_darkness[2] * (current_darkness / 255))
+    local b = 255 - math.floor(color_darkness[3] * (current_darkness / 255))
+    dark_surface:clear()
+    dark_surface:fill_color({r, g, b})
+    if darkness == current_darkness then -- Darkness reached.
+      if current_rain_mode == "storm" then -- Storm mode.
+        self:update_darkness() -- Repeat process with new random darkness value.
+      elseif darkness == 0 then -- No storm: stop darkness.
+        dark_surface = nil
+      end
+      return false
+    end
+    return true -- Keep modifying darkness value.
+  end)
+  timers["darkness_timer"]:set_suspended_with_map(false)
 end
 
 -- Return rain manager.
